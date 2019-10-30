ee108c558f460dcda16d3f27f93a3187c779bf8d
Artur Rubchenko <Artur_Rubchenko@epam.com>
2019-10-30 10:52:25 +0300

    feat: lesson 7

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,65 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { muscles, exercises } from '../store'
-import { Header, Footer } from './layouts'
-import Exercises from './exercises'
-
-// const ID_GENERATOR = () => Math.random().toString(36).substr(2, 9)
+import { muscles, exercises } from "../store";
+import { Header, Footer } from "./layouts";
+import Exercises from "./exercises";
 
 const getExercisesByGroup = exercises => {
-  return Object.entries(exercises.reduce((exercises, item) => {
-    const { muscles } = item
+  const initialExercises = muscles.reduce((exercises, category) => ({
+    ...exercises,
+    [category]: []
+  }), {});
 
-    exercises[muscles] = exercises[muscles]
-      ? [...exercises[muscles], item]
-      : [item]
+  return Object.entries(
+    exercises.reduce((exercises, item) => {
+      const { muscles } = item;
 
-    return exercises
-  }, {}))
-}
+      exercises[muscles] = exercises[muscles]
+        ? [...exercises[muscles], item]
+        : [item];
+
+      return exercises;
+    }, initialExercises)
+  );
+};
 
 export default () => {
-  const [exercisesDB, setExercisesData] = useState([])
-  const [category, setCategory] = useState('')
-  const [exercise, setExercise] = useState({})
+  const [exercisesDB, setExercisesData] = useState([]);
+  const [category, setCategory] = useState("");
+  const [exercise, setExercise] = useState({});
 
   useEffect(() => {
-    setExercisesData(exercises)
-  }, [])
+    setExercisesData(exercises);
+  }, []);
 
   const handleCategorySelect = category => {
-    setCategory(category)
-  }
+    setCategory(category);
+  };
 
   const handleExerciseSelect = id => {
-    setExercise(exercisesDB.find(exercise => exercise.id === id))
-  }
-  
+    setExercise(exercisesDB.find(exercise => exercise.id === id));
+  };
+
   const onExerciseCreate = exercise => {
-    setExercisesData([...exercisesDB, exercise])
-  }
-  
-  const transformedExercises = getExercisesByGroup(exercisesDB)
+    setExercisesData([...exercisesDB, exercise]);
+  };
+
+  const handleDeleteCategory = id => {
+    setExercisesData(exercisesDB.filter(exercise => id !== exercise.id));
+  };
+
+  const transformedExercises = getExercisesByGroup(exercisesDB);
 
   return (
     <>
-      <Header muscles={muscles} onExerciseCreate={onExerciseCreate}/>
+      <Header muscles={muscles} onExerciseCreate={onExerciseCreate} />
 
       <Exercises
         exercise={exercise}
         exercises={transformedExercises}
         category={category}
         onSelect={handleExerciseSelect}
+        onDelete={handleDeleteCategory}
       />
 
       <Footer
@@ -58,5 +68,5 @@ export default () => {
         onSelect={handleCategorySelect}
       />
     </>
-  )
-}
+  );
+};
--- src/components/exercises/index.js
+++ src/components/exercises/index.js
@@ -1,14 +1,25 @@
-import React, { Fragment } from 'react'
-import { Grid, Paper, Typography, List, ListItem, ListItemText } from '@material-ui/core'
+import React, { Fragment } from "react";
+import {
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
+
+import Delete from '@material-ui/icons/Delete';
 
 const styles = {
   Paper: {
     padding: 20,
-    margin: '10px 0',
-    height: '72vh',
-    overflow: 'auto',
+    margin: "10px 0",
+    height: "72vh",
+    overflow: "auto"
   }
-}
+};
 
 export default ({
   exercises,
@@ -16,62 +27,49 @@ export default ({
   onSelect,
   exercise: {
     id,
-    title = 'Welcome!',
-    description = 'Please select an example from the list',
+    title = "Welcome!",
+    description = "Please select an example from the list"
   },
+  onDelete,
 }) => {
   return (
     <>
       <Grid container>
         <Grid item sm>
-
           <Paper style={styles.Paper}>
             {exercises.map(([group, exercises]) =>
-              !category || category === group
-                ? <Fragment key={group}>
+              !category || category === group ? (
+                <Fragment key={group}>
                   <Typography
-                    variant='h6'
-                    style={{ textTransform: 'capitalize' }}
+                    variant="h6"
+                    style={{ textTransform: "capitalize" }}
                   >
                     {group}
                   </Typography>
                   <List component="nav" aria-label="secondary mailbox folders">
-
                     {exercises.map(({ id, title }) => (
-                      <ListItem
-                        button
-                        key={id}
-                        onClick={() => onSelect(id)}
-                      >
-                        <ListItemText
-                          primary={title}
-                        />
+                      <ListItem button key={id} onClick={() => onSelect(id)}>
+                        <ListItemText primary={title} />
+                        <ListItemSecondaryAction>
+                          <IconButton edge="end" aria-label="comments" onClick={() => onDelete(id)}>
+                            <Delete />
+                          </IconButton>
+                        </ListItemSecondaryAction>
                       </ListItem>
                     ))}
                   </List>
                 </Fragment>
-                : null
+              ) : null
             )}
           </Paper>
-
         </Grid>
         <Grid item sm>
-
           <Paper style={styles.Paper}>
-            <Typography
-              variant="h5"
-            >
-              {title}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-            >
-              {description}
-            </Typography>
+            <Typography variant="h5">{title}</Typography>
+            <Typography variant="subtitle1">{description}</Typography>
           </Paper>
-
         </Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
